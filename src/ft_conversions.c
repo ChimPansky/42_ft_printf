c81059b858f969ab03b80875ce0bd14924fe588e
Thomas Kasbarian2 <thomas.kasbarian@gmail.com>
2023-09-21 12:25:49 +0300

    added libft and provisional makefile

--- src/ft_conversions.c
+++ src/ft_conversions.c
@@ -1,6 +1,16 @@
 #include "../include/ft_printf.h"
 
-int	convert_c(char c);
-int	convert_s(char *s, char *flags);
-int	convert_d...
-// all the convert_ functions write to standard output and return number of bytes wrote to the caller function ft_printf...
+int	convert_c(int c, char *format);
+int	convert_s(char *s, char *format);
+int	convert_p(void *p, char *format);
+int	convert_d(int	i, char *format);
+int	convert_i(int	i, char *format);
+int	convert_d(int	i, char *format);
+int	convert_u(unsigned int i, char *format);
+int	convert_x(unsigned int i, char *format);
+int	convert_X(unsigned int i, char *format);
+
+
+// all the convert_ functions write to standard output and return number of bytes written to the caller function ft_printf...
+
+// use ft_putchar_fd and ft_putstr_fd
---
+++ src/test_main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	char str[5] = "abc";
+
+	char	*ptr = str;
+	void	*ptr2 = str;
+
+	printf("test: % +5.2hhd\n", 5);
+	return (0);
+
+}
